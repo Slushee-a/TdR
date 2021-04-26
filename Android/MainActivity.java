67bdaa95432363783dd1751761499b8622220661
Slushee <55996847+Slushee-a@users.noreply.github.com>
2021-04-26 22:01:53 +0200

    Motors will no turn off when app is destroyed.
    I've been working on trying to implement an mDNS-sd browser but can't figure it out. I think I will use the [BonjourBrowser]https://github.com/andriydruk/BonjourBrowser app. Maybe later on in the project I will try again. Now I will work more on mechanical part. I'm new to java and it is like nothing else I have done before.

--- Android/MainActivity.java
+++ Android/MainActivity.java
@@ -2,27 +2,22 @@
  * I'd rather add too many comments than too few.
  * Code is easier to write than it is to read, so let's not risk it.
  * Made by Slushee (Pol Fernàndez)
- * Alpha 2.0.0 (25/04/2021)
+ * Alpha 2.0.1 (26/04/2021)
  */
 
 
 //Name the package and import classes/libraries
 package com.firstapp.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -36,12 +31,13 @@ import java.util.Objects;
 //Main Activity Code
 public class MainActivity extends AppCompatActivity
 {
-    String url ="http://192.168.1.139/"; //This is temporary, for testing. I will later get the URL with mDNS-sd, which I have already implemented in the arduino code.
+    String url ="http://192.168.1.___/"; //This is temporary, for testing. I will later get the URL with mDNS-sd, which I have already implemented in the arduino code.
 
     private SeekBar seekBar1;                                                             // Define the seekbar 1 (left motor)
     private SeekBar seekBar2;                                                             // Define the seekbar 2 (right motor)
     Switch SafeMode;                                                                      // Define the Safe Mode switch
     Boolean SafeModeState = false;                                                        // Create a boolean to store the the state of the safe mode.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)                                    // When the app is running on the foreground
@@ -159,7 +155,7 @@ public class MainActivity extends AppCompatActivity
                          @Override
                          public void onErrorResponse(VolleyError error)                                                                          // If there has been an error
                          {
-                            Toast.makeText(MainActivity.this,"Failed",Toast.LENGTH_LONG).show();                                    // Warn the user that the robot is not connected
+                            Toast.makeText(MainActivity.this,"Robot is not connected.",Toast.LENGTH_LONG).show();                   // Warn the user that the robot is not connected
                          }
                      }
                 );
@@ -200,4 +196,57 @@ public class MainActivity extends AppCompatActivity
             }
         });
     }
+
+
+    @Override
+    protected void onDestroy()                                                                                                          // When the app is closed
+    {
+        super.onDestroy();
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);                                                         // Create a new request queue and
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url+"1/0", new Response.Listener<String>()              // make a request to turn of the motors. Listen for a response.
+        {
+            @Override
+            public void onResponse(String response)                                                                                      // If there is a response from the server
+            {
+                                                                                                                                         // Do nothing
+            }
+
+        },
+
+                new Response.ErrorListener()                                                                                             // Check for errors
+             {
+                 @Override
+                 public void onErrorResponse(VolleyError error)                                                                          // If there has been an error
+                 {
+                                                                                                                                         // Do nothing
+                 }
+             }
+        );
+        queue.add(stringRequest);                                                                                                        // Add the request to the queue
+
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);                                                          // Create a new request queue and
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url+"1/0", new Response.Listener<String>()               // make a request to turn of the motors. Listen for a response.
+        {
+            @Override
+            public void onResponse(String response)                                                                                       // If there is a response from the server
+            {
+                                                                                                                                          // Do nothing
+            }
+
+        },
+
+                new Response.ErrorListener()                                                                                              // Check for errors
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)                                                                        // If there has been an error
+                    {
+                                                                                                                                          // Do nothing
+                    }
+                }
+        );
+        queue.add(stringRequest);                                                                                                         // Add the request to the queue
+    }
+
 }
+
